de8f76076d348dc33bb4a481aacd6fde9008a3b1
Stefany Fernández <tefyfernandez8@gmail.com>
2022-01-04 04:24:44 +0100

    basic data changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
 
 interface IndexProps {
   title: string;
 }
 
-const index: React.FC<IndexProps> = ({title}) => (
+const index: React.FC<IndexProps> = () => (
   <React.Fragment>
     <Head>
-      <title>{title}</title>
+      <title>Hola Lolita</title>
       <link rel="stylesheet" type="text/css" href="/styles.css" />
     </Head>
-    <h1>{title}</h1>
+    <h1>Hola Lolita</h1>
   </React.Fragment>
 );
-
-/**
- * This is getServerSideProps to verify that the ENV is working,
- * nota that this functionality (because of the static nature of the data) 
- * will fit better in the getStaticProps paradigm
- */
-export const getServerSideProps: GetServerSideProps<IndexProps>  = async () => {
-  return {
-    props: {
-      title: `Hello ${process.env.PROJECT_NAME}!`,
-    },
-  }
-}
 
 export default index;
